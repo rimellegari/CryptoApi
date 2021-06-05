3358f4eecae345b9007fa28a5e6bcec31bec220d
rimellegari <rimellegari@gmail.com>
2021-06-05 16:14:52 -0300

    navbar and animations finished

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar__cont') [0]
+const navbarLinks = document.getElementsByClassName('navbar__section') [0]
 
 toggleButton.addEventListener('click', ()=>{
     navbarLinks.classList.toggle('active')
